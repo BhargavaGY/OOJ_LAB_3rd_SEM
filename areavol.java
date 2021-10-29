2b4c2be78427f10c56143032be62b7d316e525b8
BhargavaGY <84015189+BhargavaGY@users.noreply.github.com>
2021-10-29 13:00:28 +0530

    Add files via upload

---
+++ areavol.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class areavol
+{
+  public static void main(String args[])
+  {
+    int choice = 0;
+    while(choice >= 0)
+    {
+      System.out.println("MENU\n" + "1.Area and Volume of Cylinder\n" + "2.Area and volume of Cone\n");
+    }
+  }
+}
